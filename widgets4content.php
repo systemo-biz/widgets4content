69b84c86f3d70a6ac4621fef39b5ff37055ea96b
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-05-26 11:40:41 +0500

    Update widgets4content.php

--- widgets4content.php
+++ widgets4content.php
@@ -3,16 +3,13 @@
 Plugin Name: Widgets area (sidebars) for content (after and before)
 Plugin URI: https://github.com/systemo-biz/widgets4content
 Description: Widgets area for content
-Version: 20150525
+Version: 20150526
 License: GPL
 Author: Systemo
 Author URI: http://systemo.biz
 GitHub Plugin URI: https://github.com/systemo-biz/widgets4content
 GitHub Branch: master
 */
-
-//include_once 'includes/sc-swiper-s/sc-swiper-s.php';
-//include_once 'includes/meta.php';
 
 register_sidebar(array(
 	'name' => __('After content'),
@@ -35,32 +32,27 @@ register_sidebar(array(
 
 
 function wrapper_post_for_widges($content){
+	ob_start();
 	
-    if(! is_singular( 'post' )) return $content;
-    
-    ob_start();
-    ?>
-    <div class="entry_wrapper_widgets_s">
-        <div class="widgets_content_before">
-            <?php  
-                //remove_filter( 'the_content', 'wpautop' );
-                //remove_filter( 'the_excerpt', 'wpautop' );
-                dynamic_sidebar( 'content-before-widgets-s' );
-                //apply_filters( 'the_content', 'wpautop', $content );
-                //apply_filters( 'the_excerpt', 'wpautop', $content);            
-            ?>
-        </div>
-        <div class="content_wrapper_for_widgets">
-            <?php echo $content; ?>
-        </div>
-        <div class="widgets_content_after">
-            <?php dynamic_sidebar( 'content-after-widgets-s' ); ?>
-        </div>
-    </div>
-    <?php
+		?>
+		<div class="entry_wrapper_widgets_s">
+		<div class="widgets_content_before">
+		    <?php  
+		        dynamic_sidebar( 'content-before-widgets-s' );
+		    ?>
+		</div>
+		<div class="content_wrapper_for_widgets">
+		    <?php echo $content; ?>
+		</div>
+		<div class="widgets_content_after">
+		    <?php dynamic_sidebar( 'content-after-widgets-s' ); ?>
+		</div>
+		</div>
+		<?php
+	
 	$html = ob_get_contents();
 	ob_get_clean();
-    
+	
 	return $html;
 }
 add_filter('the_excerpt', 'wrapper_post_for_widges', 10, 2);
